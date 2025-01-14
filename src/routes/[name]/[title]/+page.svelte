e1a19cd8376882436cfa50fc9d71a58ceb66fbfa
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-01-14 06:50:45 -0500

    add blog post meta tags

--- src/routes/[name]/[title]/+page.svelte
+++ src/routes/[name]/[title]/+page.svelte
@@ -17,4 +17,14 @@
 <svelte:head>
 	<link rel="icon" href={data.author.avatarUrl} sizes="any" type="image/svg+xml" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css" />
+	<meta
+		property="og:url"
+		content="https://www.yap.kitchen/{data.author.username}/{data.post.slug}"
+	/>
+	<meta property="og:title" content={data.post.title} />
+	<meta property="og:description" content="a yap.kitchen blog post by {data.author.username}" />
+	<meta property="og:type" content="website" />
+	<!-- <meta property="og:image:width" content="300" />
+	<meta property="og:image:height" content="300" /> -->
+	<meta name="theme-color" content={data.author.color} />
 </svelte:head>
