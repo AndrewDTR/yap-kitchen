d0998e61ad255c90ed1c644d1b0aeae4e798fba0
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-02-04 19:50:39 -0600

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,46 @@
 <div class="content">
-	<h1>&#9658; welcome to this great place</h1>
-	<p>on yap kitchen you can yap</p>
+	<h1>&#9658; yap manifesto</h1>
+	<i>
+		<p>
+			To "yap" means to talk at length. It has a negative connotation of irritation for those in the
+			vicinity, but when confined to a metaphorical kitchen of willing participants, yapping becomes
+			a worthy and useful endeavor.
+		</p>
+		<p>
+			In a digital world in which we are increasingly bombarded with information and distractions,
+			Yap Kitchen encourages real interactions and discourse.
+		</p>
+
+		<p>
+			Read the thoughts of people, not companies or bots. Write about whatever you feel like:
+			philosophy or video games, programming or books. Yap.
+		</p>
+
+		<p>Welcome to Yap Kitchen.</p>
+
+		<p>(prelude by <a href="/gil">Gil Friedman</a>)</p>
+	</i>
+
+	<h1>&#9658; about</h1>
+	<p>
+		Hi, welcome to Yap Kitchen. This is a fun little site where you can put blog posts about the
+		topic of your choosing. Posts are rendered in markdown (with LaTeX formatting enabled, for those
+		into math typesetting) so you can yap as you please.
+	</p>
+	<p>
+		Please do not use generative artificial intelligence to write your posts. They are antithetical
+		to the site's existence.
+	</p>
+	<p>Yap Kitchen was created by <a href="/andrew">Andrew Moses</a></p>
 </div>
+
+<style>
+	p {
+		margin-top: 10px;
+		font-size: 18px;
+	}
+
+	h1 {
+		margin-top: 10px;
+	}
+</style>
