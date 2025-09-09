4b2ea64df7e81684182c6d5c182f3b4a318831d2
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-09-09 10:02:59 -0500

    change superuser imports aaaaaa

--- src/helper/superuser.js
+++ src/helper/superuser.js
@@ -1,7 +1,8 @@
 import PocketBase from "pocketbase"
 import { env } from '$env/dynamic/private';
+import { env as penv } from '$env/dynamic/public';
 
-const pb = new PocketBase(env.PUBLIC_POCKETBASE_URL);
+const pb = new PocketBase(penv.PUBLIC_POCKETBASE_URL);
 pb.autoCancellation(false);
 
 await pb.collection('_superusers').authWithPassword(env.POCKETBASE_ADMIN_EMAIL, env.POCKETBASE_ADMIN_PASSWORD, {
