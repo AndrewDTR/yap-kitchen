f240be4e4d731ebdfa3393da74b6ac931896a955
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-09-09 00:55:44 -0500

    pull commit from coolify env

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,11 +1,12 @@
-import childProcess from 'child_process';
 import { env } from '$env/dynamic/private';
 
 export async function load({ locals }) {
-	const hash = childProcess.execSync('git rev-parse --short HEAD').toString().trim();
 	const environment = env.SERVER_ENVIRONMENT;
+	const commitHash = env.COMMIT_HASH ?? 'unknown';
 
 	return {
-		commitHash: hash, environment, logUser: locals.pb.authStore.model
+		commitHash,
+		environment,
+		logUser: locals.pb?.authStore?.model
 	};
 }
